3e04063dbfda16ee82cca45d24d1fc3402828a56
knnv-ar <knnv.ar@gmail.com>
2024-03-11 10:10:38 -0300

    5. footer and navigation components [DONE]

---
+++ src/data/navData.js
@@ -0,0 +1,24 @@
+const navData = [
+  {
+    name: "2023",
+    path: "/2023/",
+  },
+  {
+    name: "2022",
+    path: "/2022/",
+  },
+  {
+    name: "2021",
+    path: "/2021/",
+  },
+  {
+    name: "2020",
+    path: "/2020/",
+  },
+  {
+    name: "About",
+    path: "/about/",
+  },
+]
+
+export default navData;
