1a5ba52ae2cc5e21ec0e6ed85548f1e3afda1b28
knnv-ar <106931704+knnv-ar@users.noreply.github.com>
2024-04-20 00:27:59 -0300

    sketch.js finally working [FIXED]

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,9 +2,11 @@
 import { z, defineCollection } from "astro:content";
 // Define a `type` and `schema` for each collection
 const exhibitionCollection = defineCollection({
-  type: 'content',
-    schema: ({ image }) => z.object({
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
       title: z.string(),
+      folderName: z.string(),
       publicationDate: z.date(),
       author: z.string(),
       imageSrc: image().refine((img) => img.width === 360, {
@@ -13,7 +15,7 @@ const exhibitionCollection = defineCollection({
       description: z.string(),
       category: z.array(z.string()),
       dependencies: z.array(z.string()),
-    })
+    }),
 });
 // Export a single `collections` object to register your collection(s)
 export const collections = {
